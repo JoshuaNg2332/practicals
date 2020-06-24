e47af0bf076028fb8280812f2b8232796c5724b4
Josh <joshua.ng2332@gmail.com>
2020-06-24 14:27:33 +0100

    Updated practicals work with Servers & Client practicals.

---
+++ week3/servers/notelist.rb
@@ -0,0 +1,7 @@
+class NoteList
+
+  def initialize
+    note = []
+  end
+
+end
--- week3/servers/servers.e1.rb
+++ week3/servers/servers.e1.rb
@@ -1,18 +1,18 @@
 require 'socket'
 
 server = TCPServer.new(2345)
-
 socket = server.accept
+socket.send('some message\000', 0)
 
-while true
+while true do
 
   socket.puts "What do you say? Type 'quit' to close connection"
-
   they_said = socket.gets.chomp
-
   socket.puts "You said: #{they_said}. Goodbye!"
+
   if (they_said == "quit")
     socket.puts "Thank you! Goodbye!"
     socket.close
   end
+  
 end
