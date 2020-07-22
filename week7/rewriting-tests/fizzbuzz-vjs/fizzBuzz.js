d859d2db627d2bacbc6e448e024d15b0cafda71b
Josh <joshua.ng2332@gmail.com>
2020-07-22 12:16:21 +0100

    first commit for rewriting tests in vanilla js for fizzbuzz.

---
+++ week7/rewriting-tests/fizzbuzz-vjs/fizzBuzz.js
@@ -0,0 +1,17 @@
+class FizzBuzz{
+  fb(input){
+  //   if (this.isDivisibleBy3(input)) {
+  //     return "Fizz";
+  //   } else if (this.isDivisibleBy5(input)){
+  //     return "Buzz";
+  //   };
+  //   return 'Fizz';
+   };
+  isDivisibleByFive(input){
+   return (input % 5 === 0);
+   };
+
+  isDivisibleByThree(input){
+    return (input % 3 === 0);
+  };
+};
---
+++ week7/rewriting-tests/fizzbuzz-vjs/fizzBuzzSpec.js
@@ -0,0 +1,22 @@
+describe('FizzBuzz', function() {
+  var fizzbuzz;
+  beforeEach(function() {
+    fizzbuzz = new FizzBuzz();
+  });
+
+  it("should return Fizz when input is 3", function() {
+    expect(fizzbuzz.fb(3)).toEqual('Fizz');
+  });
+  it("should return Buzz when input is 5", function() {
+    expect(fizzbuzz.fb(5)).toEqual('Buzz');
+  });
+
+  describe("returns true if input is divisible by ", function(){
+    it("should return false when input is 5", function() {
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+    it("should return false when input is 3", function() {
+      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+    });
+  });
+});
